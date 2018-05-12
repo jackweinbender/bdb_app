34aad3d1976783a9d8733af85e0430deb0fb797c
Jack Weinbender <jack.weinbender@gmail.com>
2018-05-11 20:47:58 -0500

    Redirect to index on title page

--- app/routes/page.js
+++ app/routes/page.js
@@ -2,6 +2,9 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     model(params){
+        if(params.page == 'title'){
+            this.transitionTo('index')
+        }
         return this.get('store').findRecord('page', params.page)
     }
 });
