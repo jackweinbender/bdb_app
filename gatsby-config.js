594231047fb88d5fa177007758af688b42e93f09
Jack Weinbender <jack.weinbender@gmail.com>
2019-08-04 10:15:19 -0500

    Site Meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Brown-Driver-Briggs Hebrew and Aramaic Lexicon of the Old Testament`,
+    description: `Brown-Driver-Briggs Hebrew and Aramaic Lexicon of the Old Testament`,
+    author: `@jackweinbender`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
