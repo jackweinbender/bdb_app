0c972b2e6410da745cddb46678a8d628b5326aa8
Jack Weinbender <jack.weinbender@gmail.com>
2019-08-04 00:41:28 -0500

    Basic page functionality.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,25 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+const path = require(`path`)
+exports.createPages = ({ graphql, actions }) => {
+    // **Note:** The graphql function call returns a Promise
+    // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
+    const { createPage } = actions
+    return graphql(`
+    {
+        allPagesJson {
+            edges {
+                node {
+                    pagenum
+                }
+            }
+        }
+    }
+    `).then(result => {
+        result.data.allPagesJson.edges.forEach(({ node }) => {
+            createPage({
+              path: `page/${node.pagenum}`,
+              component: path.resolve(`./src/components/page.js`),
+              context: { pagenum: `${node.pagenum}` },
+            })
+          })
+    })
+  }
---
+++ src/components/page.js
@@ -0,0 +1,37 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+export default ({ data }) => {
+  const page = data.pagesJson
+  const title = `BDB Page ${page.pagenum}`
+  const next = `/page/${page.next}`
+  const prev = `/page/${page.prev}`
+  const img_crop = page.img_crop || 'pct:9,2,82,96'
+
+  let url = `http://iiif.semitics-archive.org/iiif/2/bdb_emory_page-${page.pagenum}.jp2/${img_crop}/pct:60/0/gray.jpg`
+  return (
+    <Layout>
+      <SEO title={title} />
+      <h1>Page {page.pagenum}</h1>
+      <Link to={prev}>Prev</Link>
+      ||
+      <Link to={next}>Next</Link>
+      <img src={url} />
+    </Layout>
+  )}
+
+export const query = graphql`
+  query($pagenum: String) {
+    pagesJson(pagenum: { eq: $pagenum }) {
+      pagenum
+      next
+      prev
+      emory_page
+      img_crop
+    }
+  }
+`
