d84313e36351df86166aa2fe28f23cec234fc29b
Jack Weinbender <jack.weinbender@gmail.com>
2019-08-04 10:12:41 -0500

    Prune lagacy stuff

--- legacy/adapters/application.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.RESTAdapter.extend({
-    buildURL: function(record, suffix) {
-        var s = this._super(record, suffix);
-        return s + ".json";
-    }
-});
--- legacy/models/img-region.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-    top:    DS.attr('number'),
-    bottom: DS.attr('number'),
-    left:   DS.attr('number'),
-    right:  DS.attr('number')
-});
